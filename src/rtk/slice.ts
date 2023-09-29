3d35e6875cfde3a3ab48512dddd7e4a9b175fb6e
devseop <cocc263@gmail.com>
2023-09-29 19:29:23 +0900

    :art: FEAT: Redux-toolkit 추가

---
+++ src/rtk/slice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { FormStateProps, ParagraphProps } from '../types/types';
+
+const initialState: FormStateProps = {
+  paragraphNumber: 1,
+  paragraphLength: '짧게',
+  paragraphSource: '낮은 곳으로',
+  generatedParagraphs: [],
+};
+
+const formSlice = createSlice({
+  name: 'form',
+  initialState,
+  reducers: {
+    updateFormData: (state, action: PayloadAction<Partial<FormStateProps>>) => {
+      Object.assign(state, action.payload);
+    },
+    setGeneratedParagraphs: (state, action: PayloadAction<ParagraphProps>) => {
+      state.generatedParagraphs = action.payload;
+    },
+  },
+});
+
+export const { updateFormData, setGeneratedParagraphs } = formSlice.actions;
+export default formSlice.reducer;
