a62d409d8e56e8f4abb5896ca898ccb80aa0a297
devseop <cocc263@gmail.com>
2023-09-29 18:45:43 +0900

    :art: FEAT: type 설정

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export type IFormState = {
+  paragraphNumber: number;
+  paragraphLength: string;
+  paragraphSource: string;
+};
+
+export type PoemProps = {
+  author: string;
+  peom: string;
+};
+
+export type PoemListProps = PoemProps[];
