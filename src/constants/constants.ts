873611bda1bdc2fb904029a20c3254ee93638a14
devseop <cocc263@gmail.com>
2023-09-29 20:57:49 +0900

    :lipstick: 상수 및 스타일 수정

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,5 +2,6 @@ export const COLORS = {
   white: { base: 'rgba(255,255,255,1)', dimmed: 'rgba(255,255,255,0.4)' },
   green: { base: 'rgba(49, 184, 105, 1)', dimmed: 'rgba(49, 184, 105, 0.2)' },
   blue: { base: 'rgba(40, 121, 255, 1)', dimmed: 'rgba(40, 121, 255, 0.4)' },
-  gray: { base: 'rgba(80, 80, 80, 1)', dimmed: 'rgba(80, 80, 80, 0.8)' },
+  gray: { base: 'rgba(80, 80, 80, 1)', dimmed: 'rgba(80, 80, 80, 0.4)' },
+  black: { base: 'rgba(34, 34, 34, 1)', dimmed: 'rgba(34, 34, 34, 0.8)' },
 } as const;
--- src/styles/styled.ts
+++ src/styles/styled.ts
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import { COLORS } from '../constants/constants';
+// import { MdKeyboardArrowDown } from 'react-icons/md';
 
 const Container = styled.main`
   display: flex;
@@ -8,34 +9,133 @@ const Container = styled.main`
   margin: 80px auto;
 `;
 
-const HeaderText = styled.h1`
+const HeaderContainer = styled.div`
   margin-bottom: 40px;
-  text-align: center;
-  color: ${COLORS.white.base};
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  h1 {
+    font-size: 40px;
+    color: ${COLORS.white.base};
+  }
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+  }
+
+  li {
+    font-size: 16px;
+    word-spacing: 2px;
+    color: ${COLORS.white.base};
+  }
+
+  a:link,
+  a:visited {
+    font-weight: 600;
+    color: ${COLORS.blue.base};
+  }
 `;
 
 const Form = styled.form`
   display: flex;
   gap: 24px;
-  justify-content: center;
+  justify-content: flex-start;
+  align-items: flex-end;
+  line-height: 1.2;
 `;
 
-const ElementWrapper = styled.div`
+const ElementContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 4px;
+  gap: 8px;
 
   label {
     font-size: 16px;
-    font-weight: 400;
+    font-weight: 500;
     color: ${COLORS.white.dimmed};
   }
 `;
 
-const Input = styled.input``;
+const SelectContainer = styled.div``;
 
-const Select = styled.select``;
+const Input = styled.input`
+  border-radius: 6px;
+  padding: 12px 16px;
+  border: none;
+  outline: none;
+  width: 200px;
+  height: 44px;
+  font-weight: 700;
+`;
 
-const Button = styled.button``;
+const Select = styled.select`
+  /* 셀렉트 기본 css 초기화 */
+  -o-appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
 
-export const S = { Container, HeaderText, Form, ElementWrapper, Input, Select, Button };
+  ::-ms-expand {
+    display: none;
+  }
+
+  border-radius: 6px;
+  padding: 12px 16px;
+  border: none;
+  outline: none;
+  width: 200px;
+  height: 44px;
+  font-weight: 700;
+`;
+
+const Button = styled.button`
+  padding: 12px 16px;
+  width: 120px;
+  height: 46px;
+  color: white;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 1;
+  border: none;
+  border-radius: 8px;
+  background-color: ${COLORS.blue.base};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${COLORS.blue.dimmed};
+    box-shadow: 0 0 0 1px ${COLORS.blue.base} inset;
+  }
+`;
+
+const DummyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  margin: 24px 0;
+  padding: 20px 24px;
+  background-color: ${COLORS.gray.dimmed};
+  border-radius: 8px;
+  min-height: 280px;
+
+  p {
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 1.5;
+    color: ${COLORS.white.base};
+  }
+`;
+
+export const S = {
+  Container,
+  HeaderContainer,
+  Form,
+  ElementContainer,
+  SelectContainer,
+  Input,
+  Select,
+  Button,
+  DummyContainer,
+};
